f71db51ccd6cff730b8d87e88ab4ec32ec9ad6a2
Helenowl <helenowl92@yandex.ru>
2024-10-10 21:04:12 +0300

    Step 4 - gendiff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/helenowl/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/helenowl/frontend-project-46/actions)
+### Asciicast-gendiff
+[![asciicast](https://asciinema.org/a/Xb12AQR17etqfnNponTOKsO6v.svg)](https://asciinema.org/a/Xb12AQR17etqfnNponTOKsO6v)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import { readFileSync } from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 
 const readFile = (filepath) => readFileSync(filepath);
+
+const genDiff = (data1, data2) => {
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+
+  const diff = keys.flatMap((key) => {
+    if (data1[key] === data2[key]) {
+      return `    ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return keys;
+  });
+
+  return ['{', ...diff, '}'].join('\n');
+};
 
 const parser = (filepath1, filepath2) => {
   const path1 = getPath(filepath1);
@@ -12,7 +35,9 @@ const parser = (filepath1, filepath2) => {
   const path2 = getPath(filepath2);
   const data2 = JSON.parse(readFile(path2));
 
-  return console.log(data1);
+  const diff = genDiff(data1, data2);
+
+  return diff;
 };
 
 export default parser;
