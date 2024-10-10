4d7632c46b94605dcb9374274556d97c19973d59
Helenowl <helenowl92@yandex.ru>
2024-10-10 22:51:52 +0300

    Step 5 - test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { readFileSync } from 'fs';
+import parser from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
+test('parser output', () => {
+  const expected = readFile('fileOutput.txt');
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const actual = parser(filepath1, filepath2);
+  expect(actual).toEqual(expected);
+});
