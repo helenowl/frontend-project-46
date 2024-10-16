ed2bd979936ca2a23ec6a61728b0247639a36209
Helenowl <helenowl92@yandex.ru>
2024-10-16 14:23:46 +0300

    Check2-1

--- src/parsers.js
+++ src/parsers.js
@@ -4,7 +4,7 @@ const getParsers = (filepath, fileext) => {
   switch (fileext) {
     case 'json':
       return JSON.parse(filepath);
-    case 'yaml' || 'yml':
+    case ('yaml' || 'yml'):
       return yaml.load(filepath);
     default: throw new Error(`Unknown type: ${fileext}`);
   }
